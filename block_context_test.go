a9738bd1f68578df07fff6324b2924f1de21e67b
nanmu42 <i@nanmu.me>
2023-07-02 15:31:37 +0800

    fix: JSON marshaler of ContextElements

--- block_context_test.go
+++ block_context_test.go
@@ -1,6 +1,8 @@
 package slack
 
 import (
+	"encoding/json"
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,4 +20,27 @@ func TestNewContextBlock(t *testing.T) {
 	assert.Equal(t, contextBlock.BlockID, "test")
 	assert.Equal(t, len(contextBlock.ContextElements.Elements), 2)
 
+}
+
+func TestContextElements_MarshalJSON(t *testing.T) {
+	elements := ContextElements{
+		Elements: []MixedElement{
+			ImageBlockElement{
+				Type:     "image",
+				ImageURL: "https://example.com/1.jpg",
+				AltText:  "image",
+			},
+			TextBlockObject{
+				Type: "mrkdwn",
+				Text: "sample",
+			},
+		},
+	}
+
+	assert := require.New(t)
+	marshaled, err := json.Marshal(elements)
+	assert.NoError(err)
+
+	const want = `[{"type":"image","image_url":"https://example.com/1.jpg","alt_text":"image"},{"type":"mrkdwn","text":"sample"}]`
+	assert.JSONEq(want, string(marshaled))
 }
